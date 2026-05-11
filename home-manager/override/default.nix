8d8e8f5d7d2273937cc66fe332921c33541530a0
9vvert <mzhm803@gmail.com>
2026-05-11 19:42:05 +0800

    add desktop override

--- home-manager/override/default.nix
+++ home-manager/override/default.nix
@@ -1,14 +1,6 @@
 {...}:
 
 {
-  home.file.".local/share/applications/google-chrome-wayland.desktop".text = ''
-    [Desktop Entry]
-    Version=1.0
-    Name=Google Chrome (Wayland)
-    Exec=google-chrome --enable-features=UseOzonePlatform --ozone-platform=wayland %U
-    Icon=google-chrome
-    Type=Application
-    Categories=Network;WebBrowser;
-    MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/rss+xml;application/rdf+xml;
-  '';
+  home.file.".local/share/applications/google-chrome.desktop".source = ./launch/google-chrome.desktop;
+  
 }

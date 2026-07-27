b730c19c5cbaeefe2935aaa7c20bc0c2611af45c
9vvert <mzhm803@gmail.com>
2026-07-27 14:50:22 +0800

    add minecraft launcher

--- home-manager/game/default.nix
+++ home-manager/game/default.nix
@@ -4,11 +4,15 @@
   home.packages = with pkgs; [
     nethack
     angband
+    cataclysm-dda
 
     # steam related
     protonup-qt
     protontricks
     mangohud
     gamescope
+
+    # minecraft
+    prismlauncher
   ];
 }
